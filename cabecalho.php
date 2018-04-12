0703fc6083bd19d67e4000c311f516affebb535c
Gustavo Tedesco <gusviaja@gmail.com>
2018-04-12 18:55:36 -0300

    alteracao de menu -- add dropdown servicos --

--- cabecalho.php
+++ cabecalho.php
@@ -26,8 +26,17 @@
 				<div class="collapse navbar-collapse" id="myNavbar">
 				  <ul class="nav navbar-nav navbar-right">
 					<li class="first"><a href="#quemSomos" class="first scrollar">Começando</a></li>
-					<li><a href="#servicos" class="scrollar">Servicos</a></li>
-					<!-- <li><a href="#criacao-de-sites" class="scrollar">Criação de Sites</a></li> -->
+					<li><a href="#servicos" class="dropdown scrollar">O que fazemos</a></li>
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Serviços <span class="caret"></span></a>
+				          <ul class="dropdown-menu">
+				            <li><a href="#criacaoDeSite" class="scrollar">Criação de sites</a></li>
+				             <li role="separator" class="divider"></li>
+				            <li><a href="#criacaoDeSistema" class="scrollar">Software House</a></li>
+				          </ul>  
+			        </li> 
+					
 					<li><a href="#nihelpdesk" class="scrollar">Ni Helpdesk</a></li>
 					<!-- <li><a href="#contato" class="scrollar">Ni Imobiliaria</a></li> -->
 					<li><a href="https://api.whatsapp.com/send?lang=pt&phone=5511999914755" rel="no-follow" target="blank"><span class="breadcrumb" > <i class="material-icons">settings_phone</i> (11)9999-14755</span></a></li>
